d1d7fa14290c2827113a75595e620913d2f5db9f
ChantelRaja <55223546+ChantelRaja@users.noreply.github.com>
2021-09-24 11:57:32 +1000

    Add files via upload

---
+++ HW_Wk9.js
@@ -0,0 +1,4 @@
+var dotMap = "HW_Wk9.json";
+vegaEmbed("#dot_map",dotMap).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
